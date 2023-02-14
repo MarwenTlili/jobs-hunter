a8149b42c9f34b8233d9b6c37abd5f71c55d3737
MarwenTlili <marwentlili@gmail.com>
2023-02-14 14:01:42 +0100

    fix company slug

--- JobsHunter/src/Controller/CompanyController.php
+++ JobsHunter/src/Controller/CompanyController.php
@@ -3,16 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Company;
-use App\Entity\Job;
 use App\Form\CompanyType;
-use App\Form\SearchCompaniesType;
 use App\Repository\CompanyRepository;
-use App\Repository\JobRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * @Route("/company")
