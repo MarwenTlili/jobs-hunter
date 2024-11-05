79973f27a829dd0d378d9b0439c7edecb239e2af
MarwenTlili <marwentlili@gmail.com>
2024-11-05 14:04:01 +0100

    fix: rename field and add EXPERIENCE_LEVELS const

--- JobsHunter/src/Entity/Experience.php
+++ JobsHunter/src/Entity/Experience.php
@@ -8,8 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=ExperienceRepository::class)
  */
-class Experience
-{
+class Experience {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -30,7 +29,7 @@ class Experience
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
-    private $yearsOfExperience;
+    private $experienceLevel;
 
     /**
      * @ORM\Column(type="date", nullable=true)
@@ -58,102 +57,93 @@ class Experience
      */
     private $cv;
 
-    public function getId(): ?int
-    {
+    public const EXPERIENCE_LEVELS = [
+        'INTERIM' => 'Interim',
+        'JUNIOR' => 'Junior',
+        'MID' => 'Mid-Level',
+        'SENIOR' => 'Senior',
+        'LEAD' => 'Lead',
+    ];
+
+    public function getId(): ?int {
         return $this->id;
     }
 
-    public function getCompany(): ?string
-    {
+    public function getCompany(): ?string {
         return $this->company;
     }
 
-    public function setCompany(?string $company): self
-    {
+    public function setCompany(?string $company): self {
         $this->company = $company;
 
         return $this;
     }
 
-    public function getJobTitle(): ?string
-    {
+    public function getJobTitle(): ?string {
         return $this->jobTitle;
     }
 
-    public function setJobTitle(?string $jobTitle): self
-    {
+    public function setJobTitle(?string $jobTitle): self {
         $this->jobTitle = $jobTitle;
 
         return $this;
     }
 
-    public function getYearsOfExperience(): ?string
-    {
-        return $this->yearsOfExperience;
+    public function getExperienceLevel(): ?string {
+        return $this->experienceLevel;
     }
 
-    public function setYearsOfExperience(?string $yearsOfExperience): self
-    {
-        $this->yearsOfExperience = $yearsOfExperience;
+    public function setExperienceLevel(?string $experienceLevel): self {
+        $this->experienceLevel = $experienceLevel;
 
         return $this;
     }
 
-    public function getStartAt(): ?\DateTimeInterface
-    {
+    public function getStartAt(): ?\DateTimeInterface {
         return $this->startAt;
     }
 
-    public function setStartAt(?\DateTimeInterface $startAt): self
-    {
+    public function setStartAt(?\DateTimeInterface $startAt): self {
         $this->startAt = $startAt;
 
         return $this;
     }
 
-    public function getEndAt(): ?\DateTimeInterface
-    {
+    public function getEndAt(): ?\DateTimeInterface {
         return $this->endAt;
     }
 
-    public function setEndAt(?\DateTimeInterface $endAt): self
-    {
+    public function setEndAt(?\DateTimeInterface $endAt): self {
         $this->endAt = $endAt;
 
         return $this;
     }
 
-    public function getCurrent(): ?bool
-    {
+    public function getCurrent(): ?bool {
         return $this->current;
     }
 
-    public function setCurrent(?bool $current): self
-    {
+    public function setCurrent(?bool $current): self {
         $this->current = $current;
 
         return $this;
     }
 
-    public function getDescription(): ?string
-    {
+    public function getDescription(): ?string {
         return $this->description;
     }
 
-    public function setDescription(?string $description): self
-    {
+    public function setDescription(?string $description): self {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getCv(): ?CV
-    {
+    public function getCv(): ?CV {
         return $this->cv;
     }
 
-    public function setCv(?CV $cv): self
-    {
+    public function setCv(?CV $cv): self {
         $this->cv = $cv;
 
         return $this;
